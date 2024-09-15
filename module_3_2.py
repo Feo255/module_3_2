d4ac00c961db7251eaa8fce19e3ad29ce192f5d9
Feo255 <dyngafiodor@gmail.com>
2024-09-15 11:38:45 +0300

    Add files via upload

--- module_3_2.py
+++ module_3_2.py
@@ -1,7 +1,7 @@
 def send_email(message, recipient, sender = "university.help@gmail.com"):
     s_1 = recipient.count('@')
     s_2 = sender.count('@')
-    if s_1 != 0 and s_2 != 0:
+    if (s_1 == 1 and s_2 == 1) and (recipient.endswith('.ru') or recipient.endswith('.com') or recipient.endswith('.net')) and (sender.endswith('.ru') or sender.endswith('.com') or sender.endswith('.net')):
         if recipient == sender:
             print("Нельзя отправить письмо самому себе!")
         else:
